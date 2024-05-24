72dad7fd140ba399f2d016c4357052dae53a2b46
showqi <mshowqi12@gmail.com>
2024-05-24 22:25:54 +0600

    data types

---
+++ Python1.py
@@ -0,0 +1,39 @@
+# Integer (int)
+my_int = 42
+print(type(my_int))  # Output: <class 'int'>
+
+# Floating Point (float)
+my_float = 3.14
+print(type(my_float))  # Output: <class 'float'>
+
+# String (str)
+my_string = "Hello, world!"
+print(type(my_string))  # Output: <class 'str'>
+
+# Boolean (bool)
+my_bool = True
+print(type(my_bool))  # Output: <class 'bool'>
+
+# List (list)
+my_list = [1, 2, 3, 4, 5]
+print(type(my_list))  # Output: <class 'list'>
+
+# Tuple (tuple)
+my_tuple = (1, 2, 3, 4, 5)
+print(type(my_tuple))  # Output: <class 'tuple'>
+
+# Set (set)
+my_set = {1, 2, 3, 4, 5}
+print(type(my_set))  # Output: <class 'set'>
+
+# Dictionary (dict)
+my_dict = {"name": "Alice", "age": 30}
+print(type(my_dict))  # Output: <class 'dict'>
+
+# Complex Number (complex)
+my_complex = 2 + 3j
+print(type(my_complex))  # Output: <class 'complex'>
+
+# Bytes (bytes)
+my_bytes = b"Hello"
+print(type(my_bytes))  # Output: <class 'bytes'>
