92c442e24db0547e17b6578efa59b52ab8edf1f9
showqi <mshowqi12@gmail.com>
2024-05-24 22:34:29 +0600

    Casting

---
+++ python2.py
@@ -0,0 +1,12 @@
+#casting
+x="12" #now a string
+y=int(x) # converted into integer
+print(y)
+print(y+1) # adding one 
+
+a=1 #now a integer
+string= str(a)  # converted into string 
+print(string)
+
+fraction=float(a) # converted into floating value
+print(fraction)
